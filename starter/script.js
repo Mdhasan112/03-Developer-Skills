d1b2e504f7ee4c17f3aa57b93eb9ad7f997a71f3
hasan <mdhasanhk.98@gmail.com>
2022-12-02 16:31:55 +0600

    skills

--- starter/script.js
+++ starter/script.js
@@ -5,6 +5,7 @@
 // const calcAge = (birthYear) => 2037 - birthYear;
 // console.log(calcAge(2005));
 
+/*
 const temperature = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 const calcTempArray = (temp) => {
@@ -21,5 +22,56 @@ const calcTempArray = (temp) => {
   return max - min;
 };
 
+//marge 2 array
 const amplitude = calcTempArray(temperature);
 console.log(amplitude);
+
+const array1 = ['Vijendra', 'Singh'];
+const array2 = ['Singh', 'Shakya'];
+
+const countArray = (array1, array2) => {
+  const newArray = array1.concat(array2);
+  console.log(newArray);
+};
+
+countArray(array1, array2);
+*/
+/*
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'Celsius',
+    value: Number(prompt('Degree Celsius')),
+  };
+  console.log(typeof measurement.value);
+  const kelvin = measurement.value + 250;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+*/
+// const arr = ['17', '21', '23'];
+// const printForecast = function (arr) {
+//   console.log(
+//     `...${arr[0]}C in 1 day...${arr[1]}C in 2 days...${arr[2]}C in 3 days`
+//   );
+//   // for (let i = 0; i < arr.length; i++) {
+//   //   const result = arr[i];
+//   //   console.log(result);
+//   // }
+// };
+
+// printForecast(arr);
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log('...' + str);
+};
